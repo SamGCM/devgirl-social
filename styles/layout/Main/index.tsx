6ef4bfd05aa45c48ba11d54413467236f6354bda
Samuel Cruz <samuelcruz052@gmail.com>
2022-07-22 07:37:31 -0300

    created layout

---
+++ styles/layout/Main/index.tsx
@@ -0,0 +1,36 @@
+import { Background, Box } from "../../components"
+import { Divider } from "../../components/Divider"
+import Header from "../../components/Header"
+import { Sidebar } from "../../components/Sidebar"
+import { SideBarItem } from "../../components/SideBarItem"
+import { IMainLayout } from "./interface"
+
+export const MainLayout = ({
+    children
+}: IMainLayout
+): React.ReactElement => {
+    return (
+        <Background>
+            <Box
+                flex
+                flexDirection="column"
+                radius="3"
+                background="3"
+                dimensions="default"
+            >
+                <Header/>
+                <Box
+                    flex
+                >
+                    <Sidebar>
+                        <SideBarItem
+                            variant="title"
+                        >Categorias</SideBarItem>
+                        <SideBarItem>Tutoriais</SideBarItem>
+                    </Sidebar>
+                    { children }
+                </Box>
+            </Box>
+        </Background>
+    )
+}
